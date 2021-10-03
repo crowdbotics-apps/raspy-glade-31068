2cc0bf25af8519f3efe956ee7219119da148731b
Crowdbotics <team@crowdbotics.com>
2021-10-03 14:11:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "raspy_glade_31068",
+  url: "https://raspy_glade_31068.botics.co",
+  api: "https://raspy_glade_31068.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
